8b5967ad7cb781e9a6a2a97eed96973b1406f158
unknown <s111219016@stu.ntue.edu.tw>
2025-04-04 16:16:34 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router'
+import { HelmetProvider } from 'react-helmet-async'
+import { Provider } from "react-redux";
+
+import './App.css';
+import Home from './pages/Home'
+import Product from './pages/Product';
+import Category from './pages/Category'
+import store from './redux/store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <HelmetProvider context={{}}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="products">
+              <Route path="category/:categoryName" element={<Category />} />
+              <Route path="id/:productId" element={<Product />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </HelmetProvider>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/components/CartSummary.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { ShoppingBasket } from "lucide-react";
+import { selectCartItems } from "../redux/cartSlice";
+
+function CartSummary() {
+  const [isOpen, setIsOpen] = useState(false);
+  const cartItems = useSelector(selectCartItems);
+  const count = (cartItems.length > 0) ?
+              cartItems.reduce((sum, item) => sum + item.qty, 0)
+              : 0;
+  const toggleModal = () => setIsOpen(!isOpen);
+
+  return (
+    <nav
+      onClick={toggleModal}
+      className="inline-block absolute top-6 right-2 md:right-6 cursor-pointer"
+    >
+      <div className="indicator">
+        {count > 0 && <span className="indicator-item badge badge-primary text-white">{count}</span>}
+        <ShoppingBasket strokeWidth={1.5} className="w-5 h-5 md:w-6 md:h-6 text-current group-hover:scale-105 transition-transform" />
+        </div>
+      <p className="text-xs opacity-60 mt-[-4px]">Shopping bag</p>
+    </nav>
+  );
+}
+
+export default CartSummary;
